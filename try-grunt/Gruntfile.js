d295d9137b9693b8568d398bf82b49832b68097b
jasonzhuang <bluezyg@gmail.com>
2013-12-10 11:13:22 +0800

    add grunt demo

---
+++ try-grunt/Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+  console.log("begin....");
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+	  }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ["uglify"]);
+  grunt.registerTask('manual', function(target){
+	console.log("this is just a function task")
+  });
+  console.log("finish....");
+};
